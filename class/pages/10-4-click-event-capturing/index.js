cbe5be335288926e8781fc2d74e49a6cc6e87364
larmong <larmong@naver.com>
2023-03-16 03:30:23 +0900

    [ADD] event capturing

---
+++ class/pages/10-4-click-event-capturing/index.js
@@ -0,0 +1,26 @@
+export default function ClickEventCapturingPage() {
+  const aaa = (event) => {
+    event.stopPropagation()
+    alert("저는 aaa입니다!")
+  }
+  const bbb = () => {
+    alert("22222222222")
+  }
+  const ccc = () => {
+    alert("111111111111")
+  }
+
+  return (
+    <>
+      <div onClick={ccc}>
+        3
+        <div onClick={aaa}>
+          2
+          <div onClick={bbb}>
+            1
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
